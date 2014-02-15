862fe01ae5d2ecc8b1b1c91936dab54d38656fad
zainikasman <zainikasman@gmail.com>
2014-02-15 17:50:19 +0800

    added a test class

---
+++ CCTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTestViewController.h
+//  Git Test
+//
+//  Created by zainikasman on 2/15/14.
+//  Copyright (c) 2014 Tapdesk Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTestViewController : UIViewController
+
+@end
---
+++ CCTestViewController.m
@@ -0,0 +1,38 @@
+//
+//  CCTestViewController.m
+//  Git Test
+//
+//  Created by zainikasman on 2/15/14.
+//  Copyright (c) 2014 Tapdesk Innovation. All rights reserved.
+//
+
+#import "CCTestViewController.h"
+
+@interface CCTestViewController ()
+
+@end
+
+@implementation CCTestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
